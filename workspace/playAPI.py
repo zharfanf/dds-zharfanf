a23a5671a3e8411d6ef10d3db37e98812957a3e0
Chameleon Cloud User <cc@faishal-dds-server.novalocal>
2023-02-21 12:45:03 +0000

    adding api

--- workspace/playAPI.py
+++ workspace/playAPI.py
@@ -18,6 +18,7 @@ def index():
     
     # Updating the configuration file
     os.system("yq -i '.default.video_name = %s' configuration.yml" %(video_name))
+    # os.system("yq -i '.default.video_name = %s' configuration.yml" %(video_name))
 
     # Executing the program
     subprocess.run(["python", "entrance.py"])
